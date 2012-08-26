033f3f81d2c006be5fa24f534d6b1a90ce465181
Thomas Bartelmess <tbartelmess@marketcircle.com>
2012-08-26 19:00:01 -0400

    Initial Commit

---
+++ PGReader/MCDocument.h
@@ -0,0 +1,13 @@
+//
+//  MCDocument.h
+//  PGReader
+//
+//  Created by Thomas Bartelmess on 12-08-26.
+//  Copyright (c) 2012 marketcircle. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MCDocument : NSDocument
+
+@end
---
+++ PGReader/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  PGReader
+//
+//  Created by Thomas Bartelmess on 12-08-26.
+//  Copyright (c) 2012 marketcircle. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
